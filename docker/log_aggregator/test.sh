02b369664c9b3521d5e32747dab8014ab26bafc0
house <dhouse@mozilla.com>
2019-05-03 23:40:26 -0600

    use read-logs in test

--- docker/log_aggregator/test.sh
+++ docker/log_aggregator/test.sh
@@ -1,5 +1,6 @@
 docker-compose up -d --remove-orphans
 docker-compose exec rsyslog-client sh -c 'logger "client hostname:$(hostname)"'
 docker-compose exec rsyslog-client sh -c 'for level in DEBUG INFO NOTICE WARN ERR CRIT ALERT EMERG; do logger -p $level test_${level}_message; done'
-docker-compose logs --tail=100 log-aggregator
+docker-compose logs -f --tail=10 log-aggregator
+docker-compose logs -f --tail=10 read-logs
 docker-compose down
